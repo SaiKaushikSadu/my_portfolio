b06fbe487f7a3e0d8cbaca408126fd434846963b
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-11-18 19:27:23 +0530

    update next.config added output

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  output: "export", // Required for static export
+  trailingSlash: true, // Ensures correct routing for static deployment
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "", // Optional: for deploying on a subpath like "/portfolio"
 };
 
 module.exports = nextConfig;
