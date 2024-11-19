79445c186da36441c692d2f0e86c317314c7a9b6
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-11-19 11:21:45 +0530

    added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,11 @@ import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
+import Script from 'next/script'; 
 
 const nunito = Nunito({ subsets: ["latin"] });
+
+const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_KEY; 
 
 export const metadata = {
   title: "Sai Kaushik Sadu",
@@ -19,6 +22,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        {/* Google Analytics Scripts */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${nunito.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
